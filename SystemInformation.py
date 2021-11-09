ced2286c7fb45e76d25e834f08f02fa4ca74222b
Md. Rakibul Islam <mdrakibulislam.zero@gmail.com>
2021-11-09 09:51:07 +0600

    Add files via upload

---
+++ SystemInformation.py
@@ -0,0 +1,124 @@
+import psutil
+import platform
+import GPUtil
+from tabulate import tabulate
+from datetime import datetime
+
+
+def get_size(bytes, suffix="B"):
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
+
+
+# System Information
+print("=" * 40, "System Information", "=" * 40)
+uname = platform.uname()
+print(f"System: {uname.system}")
+print(f"Node Name: {uname.node}")
+print(f"Release: {uname.release}")
+print(f"Version: {uname.version}")
+print(f"Machine: {uname.machine}")
+print(f"Processor: {uname.processor}")
+
+# Boot Time
+print("=" * 40, "Boot Time", "=" * 40)
+boot_time_timestamp = psutil.boot_time()
+bt = datetime.fromtimestamp(boot_time_timestamp)
+print(f"Boot Time: {bt.year}/{bt.month}/{bt.day} {bt.hour}:{bt.minute}:{bt.second}")
+
+# CPU Information
+print("=" * 40, "CPU Information", "=" * 40)
+# Number of Cores
+print("Physical cores:", psutil.cpu_count(logical=False))
+print("Total cores:", psutil.cpu_count(logical=True))
+# CPU Frequencies
+cpufreq = psutil.cpu_freq()
+print(f"Maximum Frequency: {cpufreq.max:.2f}Mhz")
+print(f"Minimum Frequency: {cpufreq.min:.2f}Mhz")
+print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
+# CPU Usage
+print("CPU Usage Per Core:")
+for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
+    print(f"Core {i}: {percentage}%")
+print(f"Total CPU Usage: {psutil.cpu_percent()}%")
+
+# Memory Information
+print("=" * 40, "Memory Information", "=" * 40)
+# Memory Details
+svmem = psutil.virtual_memory()
+print(f"Total: {get_size(svmem.total)}")
+print(f"Available: {get_size(svmem.available)}")
+print(f"Used: {get_size(svmem.used)}")
+print(f"Percentage: {svmem.percent}%")
+
+# SWAP Memory Information
+print("=" * 40, "SWAP Memory Information", "=" * 40)
+# SWAP Memory Details
+swap = psutil.swap_memory()
+print(f"Total: {get_size(swap.total)}")
+print(f"Free: {get_size(swap.free)}")
+print(f"Used: {get_size(swap.used)}")
+print(f"Percentage: {swap.percent}%")
+
+# Disk Information
+print("=" * 40, "Disk Information", "=" * 40)
+print("Partitions and Usage:")
+# Disk Partitions
+partitions = psutil.disk_partitions()
+for partition in partitions:
+    print(f"=== Device: {partition.device} ===")
+    print(f"  Mountpoint: {partition.mountpoint}")
+    print(f"  File system type: {partition.fstype}")
+    try:
+        partition_usage = psutil.disk_usage(partition.mountpoint)
+    except PermissionError:
+        continue
+    print(f"  Total Size: {get_size(partition_usage.total)}")
+    print(f"  Used: {get_size(partition_usage.used)}")
+    print(f"  Free: {get_size(partition_usage.free)}")
+    print(f"  Percentage: {partition_usage.percent}%")
+# Disk IO Statistics
+disk_io = psutil.disk_io_counters()
+print(f"Total read: {get_size(disk_io.read_bytes)}")
+print(f"Total write: {get_size(disk_io.write_bytes)}")
+
+# Network Information
+print("=" * 40, "Network Information", "=" * 40)
+if_addrs = psutil.net_if_addrs()
+for interface_name, interface_addresses in if_addrs.items():
+    for address in interface_addresses:
+        print(f"=== Interface: {interface_name} ===")
+        if str(address.family) == 'AddressFamily.AF_INET':
+            print(f"  IP Address: {address.address}")
+            print(f"  Netmask: {address.netmask}")
+            print(f"  Broadcast IP: {address.broadcast}")
+        elif str(address.family) == 'AddressFamily.AF_PACKET':
+            print(f"  MAC Address: {address.address}")
+            print(f"  Netmask: {address.netmask}")
+            print(f"  Broadcast MAC: {address.broadcast}")
+# Network IO Statistics
+net_io = psutil.net_io_counters()
+print(f"Total Bytes Sent: {get_size(net_io.bytes_sent)}")
+print(f"Total Bytes Received: {get_size(net_io.bytes_recv)}")
+
+# GPU Information
+print("=" * 40, "GPU Details", "=" * 40)
+gpus = GPUtil.getGPUs()
+list_gpus = []
+for gpu in gpus:
+    gpu_id = gpu.id
+    gpu_name = gpu.name
+    gpu_load = f"{gpu.load * 100}%"
+    gpu_free_memory = f"{gpu.memoryFree}MB"
+    gpu_used_memory = f"{gpu.memoryUsed}MB"
+    gpu_total_memory = f"{gpu.memoryTotal}MB"
+    gpu_temperature = f"{gpu.temperature} °C"
+    gpu_uuid = gpu.uuid
+    list_gpus.append((gpu_id, gpu_name, gpu_load, gpu_free_memory, gpu_used_memory,
+                       gpu_total_memory, gpu_temperature, gpu_uuid))
+
+print(tabulate(list_gpus, headers=("ID", "Name", "Load", "Free Memory", "Used Memory", "Total Memory",
+                                   "Temperature", "UUID")))
